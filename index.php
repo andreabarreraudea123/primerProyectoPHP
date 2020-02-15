be3f2160f8561aa1f0df88e7b989cf4832ede46c
501 <501@academicos.cesde.edu.co>
2020-02-14 21:01:18 -0500

    esta es la version funcional de mi codigo

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+//esto es un comentario de linea
+
+/*Esto es un comentario
+de varias lineas
+fjfkgjkfjgk*/
+
+//ejemplos de variables
+$nombre="Andrea Barrera ";
+$edad=31;
+$estatura=1.55;
+
+//para imprimir una variable
+echo($nombre);
+echo($edad);
+
+//para concatenar
+
+print("<br>");
+
+echo("Mi Nombre es ".$nombre."y la edad es:" .$edad);
+
+
+
+
+?>
